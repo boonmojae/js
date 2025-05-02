14f4f1141bde945d2151f10e0b8cf1440d14deb9
boonmojae <pminny99@naver.com>
2025-05-02 18:27:59 +0900

    commit

--- operator.js
+++ operator.js
@@ -1,5 +1,33 @@
-let result1 = 2 + 3 * 4;
-let result2 = (2 + 3) * 4;
+// let result1 = 2 + 3 * 4;
+// let result2 = (2 + 3) * 4;
 
-console.log(result1);
-console.log(result2);
+// console.log(result1);
+// console.log(result2);
+
+// let a = 5;
+// let b = 5;
+
+// console.log("a = " + a + ", " + ++a);
+// console.log("a = " + a);
+
+// console.log("b = " + b + ", " + b--);
+// console.log("b = " + b);
+
+console.log(!true);
+console.log(!false);
+console.log(!0);
+console.log(!"hello");
+console.log(!undefined);
+console.log(!!"hello");
+console.log(!!0);
+
+console.log("------------------");
+
+console.log(~5);
+console.log(~0);
+console.log(~-1);
+
+console.log("------------------");
+
+console.log(2 + 3 * 4 + 5 % 2);
+console.log(3 % 4 * 5);
