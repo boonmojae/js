ee8a18b6102d19af8dde3f2681f04bf1af1584a3
boonmojae <pminny99@naver.com>
2025-05-13 22:20:37 +0900

    boot commit

--- 0512/test.js
+++ 0512/test.js
@@ -1,35 +1,54 @@
- document.querySelector(".btn.btn-primary").addEventListener('click', function(event) {
+document.querySelector(".btn.btn-primary").addEventListener('click', function(event) {
     event.preventDefault();
 
     const list = document.getElementById("messageList");
 
-    const username = document.getElementById("username").value;
-    const email = document.getElementById("email").value;
-    const message = document.getElementById("message").value;
+    const usernameInput = document.getElementById("username");
+    const emailInput = document.getElementById("email");
+    const messageInput = document.getElementById("message");
+
+    const username = usernameInput.value;
+    const email = emailInput.value;
+    const message = messageInput.value;
 
     console.log(username);
     console.log(email);
     console.log(message);
 
     if (!username || !email || !message) {
-      alert("입력해주세요");
+      alert("정보를 입력해주세요");
       return;
     }
 
     const newMes = document.createElement("li");
+    newMes.classList.add("list-group-item");
+    newMes.style.position = "relative";
+
+    // li안에 x버튼
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "X";
+    deleteBtn.classList.add("btn", "btn-danger", "btn-sm");
+
+    // x버튼 위치
+    deleteBtn.style.position = "absolute";
+    deleteBtn.style.top = "5px";
+    deleteBtn.style.right = "20px";//position 사용할때 위치 지정
+
+    deleteBtn.addEventListener("click", function () {
+        list.removeChild(newMes);
+    });
+
     newMes.innerText = `${message} ${username} ${email}`;
+    newMes.append(deleteBtn);
 
     list.append(newMes);
 
-    
-  })
-  
-  function clearForm() {
-  const username = document.getElementById("username");
-  const email = document.getElementById("email");
-  const message = document.getElementById("message");
+    usernameInput.value = "";
+    emailInput.value = "";
+    messageInput.value = "";
+});
 
-  username.value = "";
-  email.value = "";
-  message.value = "";
+function clearForm() {
+    const list = document.getElementById("messageList");
+    list.innerHTML = "";
 }
